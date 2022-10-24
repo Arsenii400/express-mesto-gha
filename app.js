b245665e4dce26e2923d092b85af1d5510285ba8
Arsenii Machekhin <arsenium2@gmail.com>
2022-10-24 21:37:11 +0300

    Исправил обработку неправильного пути

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const user = require('./routes/users');
@@ -15,14 +16,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '6356967d6f8f7b5628cdcd74',
+    _id: '63567cf4709494719370014a',
   };
 
   next();
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', user);
 app.use('/cards', card);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый путь не найден' });
+});
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
